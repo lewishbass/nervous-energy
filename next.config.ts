b06cfb2f61daef601781bba2492cec0786e1a4bf
lewishbass <66450010+lewishbass@users.noreply.github.com>
2025-03-24 00:34:31 -0400

    mathjax is the problem

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  transpilePackages: ['xmldom-sre'],
+  transpilePackages: ['xmldom-sre', 'mathjax-full'],
 };
 
 export default nextConfig;
